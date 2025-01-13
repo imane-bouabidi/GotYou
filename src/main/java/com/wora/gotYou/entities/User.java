e0a55951ae53f64885e5b90bbb4e9fbc6402e74c
imane-bouabidi <imaneyoucode@gmail.com>
2025-01-13 10:26:14 +0100

    user entity

--- src/main/java/com/wora/gotYou/entities/User.java
+++ src/main/java/com/wora/gotYou/entities/User.java
@@ -1,5 +1,56 @@
 package com.wora.gotYou.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @NotBlank(message = "The name must not be null")
+    @Size(min = 5, max = 20, message = "Name must be between 5 and 20 characters")
+    protected String name;
+
+    @NotBlank(message = "The last name must not be null")
+    @Size(min = 5, max = 20, message = "Last name must be between 5 and 20 characters")
+    protected String lastName;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Email must be valid")
+    protected String email;
+
+    @NotBlank(message = "Nickname must not be blank")
+    @Size(min = 3, max = 15, message = "Nickname must be between 3 and 15 characters")
+    protected String nickname;
+
+    @NotBlank(message = "CIN must not be blank")
+    @Pattern(regexp = "^[A-Za-z0-9]{5,10}$", message = "CIN must be alphanumeric and between 5 and 10 characters")
+    protected String cin;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    protected String password;
+
+    @NotNull(message = "Birth date must not be null")
+    @Past(message = "Birth date must be in the past")
+    protected LocalDate birthDate;
+
+    protected LocalDate inscriptionDate;
 }
